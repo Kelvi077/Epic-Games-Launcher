772b282a79e17d1136360c1f9f9f22e55efe3178
Kelvi077 <kelvisaraci20@gmail.com>
2024-10-29 11:32:16 +0100

    update spotlight

---
+++ script.js
@@ -0,0 +1,23 @@
+// hover on discover
+$(".discover-text").hover(
+  function () {
+    $(".discover-text-2").css("position", "relative");
+    $(".discover-text-2").css("left", "0.5em");
+  },
+  function () {
+    $(".discover-text-2").css("position", "relative");
+    $(".discover-text-2").css("left", "0em");
+  }
+);
+
+// hover on spotlight-text
+$(".spotlight-text").hover(
+  function () {
+    $(".spotlight-text-2").css("position", "relative");
+    $(".spotlight-text-2").css("left", "0.5em");
+  },
+  function () {
+    $(".spotlight-text-2").css("position", "relative");
+    $(".spotlight-text-2").css("left", "0em");
+  }
+);
